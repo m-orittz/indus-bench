497f2fcbb047fa37caec1445dfde108086f57cee
Moritz W <moritz2303@hotmail.com>
2025-08-30 21:32:57 +0200

    Update working hello-mani.py test file, gitignore bugged dirs

--- test/hello-mani.py
+++ test/hello-mani.py
@@ -3,14 +3,17 @@
 import os, time, glob
 import gymnasium as gym
 import mani_skill.envs
-from mani_skill.utils.wrappers.record import RecordEpisode
+from mani_skill.utils import gym_utils
+from mani_skill.utils.wrappers import RecordEpisode
 
+# Something is broken, why is there a '~' folder now?
 OUTPUT_DIR = os.path.expanduser("~/Videos/sim-output")
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 env = gym.make(
     "PickCube-v1", # there are more tasks e.g. "PushCube-v1", "PegInsertionSide-v1", ...
     num_envs=1,
+    max_episode_steps=200,
     obs_mode="rgb", # there is also "state_dict", "rgbd", "state"...
     control_mode="pd_ee_delta_pose", # there is also "pd_joint_delta_pos", ...
     render_mode="rgb_array",
@@ -22,9 +25,12 @@ env = RecordEpisode(
         env,
         output_dir=OUTPUT_DIR,
         save_trajectory=False,
-        trajectory_name=f"hello-test-1",
+        info_on_video=False,
+        #trajectory_name=f"hello-test-1",
         save_video=True,
-        video_fps=30
+        #video_fps=30,
+        max_steps_per_video=gym_utils.find_max_episode_steps_value(env),
+        render_substeps=True
     )
 
 print("Observation space", env.observation_space)
@@ -43,16 +49,17 @@ while not done:
 env.reset()
 
 # None of the options below work, maybe check discord?
+# Turns out vlc is broken ... WHY?
 # Now, explicitly tell the RecordEpisode wrapper to finish ffmpeg
-print("Recording complete, finalizing video to disk…")
-if hasattr(env, "flush"):
-    env.flush()   # <- this waits for video writer to close
-
-# Let I/O flush, mimic CLI pause
 # print("Recording complete, finalizing video to disk…")
-# time.sleep(5)
+# if hasattr(env, "flush"):
+#     env.flush()   # <- this waits for video writer to close
 
-# env.close()
+# Let I/O flush, mimic CLI pause - probably not required - vlc's fault
+print("Recording complete, finalizing video to disk…")
+time.sleep(2)
+
+env.close()
 
 # env.reset()
 # for n in range(200):
